42890d32411126035bb462aa1b35fd2e7be88599
DmalTzi <Audyomg@gmail.com>
2023-04-19 23:34:15 +0700

    First commit

---
+++ hiragana.py
@@ -0,0 +1,11 @@
+import random
+
+selects = []
+
+def section1():
+    """Input and select a ka sa ta na ha ma ya ra wa n"""
+    select = input("Input")
+
+    selects.append(select)
+
+print(selects)
