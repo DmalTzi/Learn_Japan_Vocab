541a14be00ddf00bf89993944791f65744d1ecbc
DmalTzi <Audyomg@gmail.com>
2023-04-20 01:56:26 +0700

    complete v.0.02

--- hiragana.py
+++ hiragana.py
@@ -1,32 +1,58 @@
 import random
 from data import Hiragana, CompareHandK
-selects = []
-after_select = []
 
 
-while True:
-    select = input("Input : ").lower()
-    if select == "0":
-        break
-    if select in selects:
-        continue
-    else:
-        selects.append(select)
+Punctuation_Select = []
+Shuffled_Punctuation = []
+Run_Program = True
 
-def shuffle_latter(i):
-    getlatter = Hiragana[i]
+def shuffle_latter(Punctuation_selected):
+    """"Shuffle punctuation from your select"""
+    getlatter = Hiragana[Punctuation_selected]
     random.shuffle(getlatter)  
     return getlatter  
 
-for i in selects:
-    for j in shuffle_latter(i):
-        after_select.append(j)
 
-for i in after_select:
-    user = input(f"{i} Input Your guess : ").lower()
-    if CompareHandK[user] == i:
-        print("You right")
-    else:
-        print("You wrong")
-print(after_select)
-print(selects)
+def check_vocab(latter):
+    for i in latter:
+        user = input(f"{i} Input Your guess : ").lower()
+        try:
+            if CompareHandK[user] == i:
+                print("You right")
+            else:
+                print("You wrong")
+        except Exception as e:
+            print("Not have in this program's vacab")
+            print(e)
+
+
+def shuffle_answer(Punctuation_Select):
+    for Punctuation_char in Punctuation_Select:
+        for Shuffled_latter in shuffle_latter(Punctuation_char):
+            Shuffled_Punctuation.append(Shuffled_latter)
+
+    return Shuffled_Punctuation
+
+while Run_Program:
+    while True:
+        select = input("Input : ").lower()
+        if select == "0":
+            break
+        if select in Punctuation_Select:
+            continue
+        else:
+            Punctuation_Select.append(select)
+
+    Shuffled_answer = shuffle_answer(Punctuation_Select)
+    
+    random.shuffle(Shuffled_answer)
+
+    check_vocab(Shuffled_answer)
+
+    if input("Want to play again? Type 'y' or 'n' : ").lower() == 'n':
+        print("End this program")
+        Run_Program = False
+
+
+print(Shuffled_Punctuation)
+print(Punctuation_Select)
